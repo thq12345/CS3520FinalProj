d3df94f77ce1b7385b5bfc14c26f6a2d7ef64f75
Tianhao Qu <qu.t@northeastern.edu>
2023-03-24 15:29:25 -0400

    update

--- main.cpp
+++ main.cpp
@@ -1,18 +1,50 @@
 #include <iostream>
 #include "eigen-3.4.0/Eigen/Dense"
- 
-using Eigen::MatrixXd;
+#include <fstream>
+#include <vector>
 
-//quick example trying if eigen is working
-int main()
-{
-  MatrixXd m(2,2);
-  m(0,0) = 3;
-  m(1,0) = 2.5;
-  m(0,1) = -1;
-  m(1,1) = m(1,0) + m(0,1);
-  std::cout << m << std::endl;
-  return 0;
+using namespace Eigen;
+using namespace std;
+
+bool contains_na(const std::string& str) {
+  return str.find("N/A") != std::string::npos;
+}
+
+Eigen::MatrixXd openData(string fileToOpen){
+  vector<double> matrixEntries;
+  ifstream matrixDataFile(fileToOpen);
+  string matrixRowString;
+  string matrixEntry;
+
+  int matrixRowNumber = 0;
+  while (getline(matrixDataFile, matrixRowString)){
+    if (contains_na(matrixRowString)) {
+      continue;
+    }
+    stringstream matrixRowStringStream(matrixRowString);
+    int ct = 0;
+    while (getline(matrixRowStringStream, matrixEntry, ',')){
+      if (ct == 0) {
+        std::stringstream datess(matrixEntry);
+        std::string token;
+        while (std::getline(datess, token, '/')) {
+          matrixEntries.push_back(stod(token));
+        }
+      } else {
+        matrixEntries.push_back(stod(matrixEntry));
+      }
+      ct += 1;
+    }
+    matrixRowNumber++;
+  }
+  return Map<Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(matrixEntries.data(),matrixRowNumber, matrixEntries.size() / matrixRowNumber);
 }
 
 
+// quick example trying if eigen is working
+int main()
+{
+  MatrixXd test_data = openData("gold.csv");
+  cout << test_data;
+  return 0;
+}
